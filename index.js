7ed8c3112c8058c6436ee0f5b27da454e6cb31e5
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-01 16:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return object.assign({},object,{[key]:value});
   
 }
- function updateObjectWithKeyAndValue(object,key2,value2) {
-   
-   object[key2] = value2;
-   return object;
- }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+}
