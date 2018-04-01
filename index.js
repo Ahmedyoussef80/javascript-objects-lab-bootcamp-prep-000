d8cffb881dc1feb17e950fa7a424d8577dd29437
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-01 16:44:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+  object[key] = value;
+  return object;
+}
+ function updateObjectWithKeyAndValue(object,key2,value2) {
+   
+   object[key2] = value2;
+   return object;
+ }
