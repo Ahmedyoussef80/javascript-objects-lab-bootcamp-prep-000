35264d8633fc83593eb131f8ec97d1feb5b1092e
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-01 17:08:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {};
-function updateObjectWithKeyAndValue(object, key, value) {
-  return object.assign({},object,{[key]:value});
+
   
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
